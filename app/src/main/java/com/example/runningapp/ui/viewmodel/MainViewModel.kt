0a799bd86d035b26010bfa8101cc253db2f139dc
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-01-05 23:40:08 +0700

    show run on recycler view

--- app/src/main/java/com/example/runningapp/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/runningapp/ui/viewmodel/MainViewModel.kt
@@ -13,6 +13,8 @@ class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
 
+    val runSortedByDate = mainRepository.getAllRunSortedByDate()
+
     fun insertRun(run: Run) = viewModelScope.launch {
         mainRepository.insertRun(run)
     }
